6dea475db516399d474e270ffb25e78aa0d74bd3
Dipesh Bajgain <dipbazz@gmail.com>
2021-02-04 19:50:02 +0545

    Add user and friendship associations
    Add methods #friends, #pending_friends, #friend_requests, #friends?
    and #confirm_friend in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,30 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
+
+  def friends
+    friend_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+    friend_array + inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
+    friend_array.compact
+  end
+
+  def pending_friends
+    friendships.map { |friendship| friendship.friend if !friendship.confirmed }.compact
+  end
+
+  def friend_requests
+    friendships.map { |friendship| friendship.user if !friendship.confirmed }.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find { |friendship| friendship.user == user }
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friends?(user)
+    friends.include?(user)
+  end
 end
